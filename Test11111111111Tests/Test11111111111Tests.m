a46dc4119152da9115a84b04d58b0b516d5dfa0f
jiangzenghui005@163.com <jiangzenghui005@163.com>
2019-01-03 14:13:36 +0800

    Initial Commit

---
+++ Test11111111111Tests/Test11111111111Tests.m
@@ -0,0 +1,37 @@
+//
+//  Test11111111111Tests.m
+//  Test11111111111Tests
+//
+//  Created by ys on 2019/1/3.
+//  Copyright © 2019 mg. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Test11111111111Tests : XCTestCase
+
+@end
+
+@implementation Test11111111111Tests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
